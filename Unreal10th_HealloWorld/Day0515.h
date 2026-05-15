950af8d1fcbbe4b6417541577107eb432b13482b
go2665 <go2665@outlook.com>
2026-05-15 17:07:27 +0900

    5/15 수업내용. 단축키와 printf 사용법

---
+++ Unreal10th_HealloWorld/Day0515.h
@@ -0,0 +1,27 @@
+#pragma once
+
+// '//' Comment 주석 : 실행파일에 포함이 되지 않는 코드
+/*
+* 주석
+* 주석주석주석
+* 주석주석
+*/
+
+// 단축키
+// Ctrl+D : 한줄 복제하기
+// Ctrl+화살표 : 단어 단위로 이동하기
+// Alt+Shift+화살표 : 세로로 블럭 잡기
+// Ctrl+Space : 자동완성창 띄우기(인텔리센스)
+// Alt+화살표 위아래 : 현재 줄을 위아래로 옮기기
+// F5 : 디버그로 시작
+// F9 : 브레이크 포인트 설정/해제
+
+/*
+* printf 사용 예시
+    printf("Hello World!\n나는 고병조입니다.\n");       // \n 줄바꿈
+    printf("Hello World!\t나는 고병조입니다.\n");       // \t 탭
+    printf("Hello\t\t나는 고병조입니다.\n");
+    printf("\'Hello World!\'\n");   // 작은 따옴표
+    printf("\"Hello World!\"\n");   // 큰 따옴표
+    printf("\\Hello World!\\\n");   // \
+*/
--- Unreal10th_HealloWorld/Unreal10th_HealloWorld.cpp
+++ Unreal10th_HealloWorld/Unreal10th_HealloWorld.cpp
@@ -3,10 +3,12 @@
 
 #include <iostream>
 
-int main()
+
+
+int main()  // 코드의 시작점
 {
-    std::cout << "Hello World!\n";
-    // 블라블라
+
+    
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
