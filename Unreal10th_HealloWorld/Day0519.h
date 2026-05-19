0c4aafc38a3d17bee236261a033a25806287ec0e
go2665 <go2665@outlook.com>
2026-05-19 17:06:44 +0900

    실습풀이

--- Unreal10th_HealloWorld/Day0519.h
+++ Unreal10th_HealloWorld/Day0519.h
@@ -192,3 +192,248 @@
 //{
 //	printf("B학점입니다.\n");
 //}
+
+
+	// 조건문 간단 실습(입력은 정수만)
+	// 1. 숫자를 입력받아 양수인지 음수인지 0인지 판단하는 코드
+	//int Number = 0;
+	//printf("확인할 숫자를 입력해 주세요 : ");
+	//cin >> Number;
+	//if (Number > 0)	// 조건문에서는 항상 범위가 큰 것을 먼저 체크하는 것이 좋다.
+	//{
+	//	printf("%d는 양수입니다.\n", Number);
+	//}
+	//else if (Number < 0)
+	//{
+	//	printf("%d는 음수입니다.\n", Number);
+	//}
+	//else
+	//{
+	//	printf("%d는 0입니다.\n", Number);
+	//}
+	// 테스트는 경계값과 중간값을 꼭 체크해야한다.
+
+	// 2. 숫자를 입력받아 홀수인지 짝수인지 판단하는 코드
+	//printf("홀수인지 짝수인지 확인할 숫자를 입력해 주세요 : ");
+	//cin >> Number;
+	//if (Number % 2 == 0)
+	//{
+	//	printf("짝수입니다.\n");
+	//}
+	//else
+	//{
+	//	printf("홀수입니다.\n");
+	//}
+
+	// 3. 두 수를 입력 받아 더 큰 수를 출력하는 코드. 같을 경우 같다고 출력.
+	//int Number2 = 0;
+	//printf("숫자 두개를 입력해 주세요 : ");
+	//cin >> Number >> Number2;
+	//if (Number > Number2)
+	//{
+	//	printf("%d와 %d 중에 %d가 더 큽니다.\n", Number, Number2, Number);
+	//}
+	//else if (Number < Number2)
+	//{
+	//	printf("%d와 %d 중에 %d가 더 큽니다.\n", Number, Number2, Number2);
+	//}
+	//else
+	//{
+	//	printf("%d와 %d는 같은 수입니다.\n", Number, Number2);
+	//}
+
+
+	// 논리 연산자 간단 실습
+	// 1. 나이와 키를 입력 받아, 6세 이상, 120cm 이상일 때 롤러코스터 탑승 가능, 그 외에는 불가능으로 출력하기
+	//int Age = 0;
+	//int Height = 0;
+	//const int BaseAge = 6;
+	//const int BaseHeight = 120;
+	//printf("나이와 키를 입력해 주세요 : ");
+	//cin >> Age >> Height;
+	//if (Age >= 6 && Height >= 120)
+	//{
+	//	printf("롤러코스터를 탈 수 있습니다!\n");
+	//}
+	//else
+	//{
+	//	printf("롤러코스터를 탈 수 없습니다.\n");
+	//}
+
+	//// (Age >= 6 && Height >= 120)
+	//// !(Age >= 6 && Height >= 120)
+	//// Age < 6 || Height < 120
+
+
+	// 2. 점수를 입력 받아 90점 이상은 A, 80점 이상은 B, 70점 이상은 C, 60점 이상은 D, 그 이하는 F라고 출력하기
+	//const int GradeA = 90;
+	//const int GradeB = 80;
+	//const int GradeC = 70;
+	//const int GradeD = 60;
+	//int Score = 0;
+	//printf("점수를 입력해 주세요 : ");
+	//cin >> Score;
+	//printf("입력한 점수는 %d입니다.\n", Score);
+	//std::string Grade;
+	//if (Score >= GradeA)
+	//{
+	//	Grade = "A학점";
+	//}
+	//else if(Score >= GradeB)
+	//{
+	//	Grade = "B학점";
+	//}
+	//else if (Score >= GradeC)
+	//{
+	//	Grade = "C학점";
+	//}
+	//else if (Score >= GradeD)
+	//{
+	//	Grade = "D학점";
+	//}
+	//else
+	//{
+	//	Grade = "F학점";
+	//}
+	//printf("당신의 학점은 [%s]입니다.", Grade.c_str());
+
+	// 3. 세 과목의 점수를 입력받아, 세 과목의 평균점이 60점 이상이면 "합격", 아니면 "불합격"을 출력하는 프로그램을 작성하기
+	//	  단 한과목이라도 40점 미만이면 불합격 출력하기
+	//int Score1 = 0, Score2 = 0, Score3 = 0;
+	//printf("세 과목의 점수를 입력해 주세요 : ");
+	//cin >> Score1 >> Score2 >> Score3;
+	//printf("입력된 점수는 다음과 같습니다.\n[%d], [%d], [%d]\n", Score1, Score2, Score3);
+
+	//const int PassScore = 60;
+	//const int FailScore = 40;
+	//if (Score1 < FailScore || Score2 < FailScore || Score3 < FailScore)
+	//{
+	//	printf("불합격 입니다.\n");
+	//}
+	//else 
+	//{
+	//	float Average = (Score1 + Score2 + Score3) / 3;
+	//	const float Threshold = 0.0001f;
+	//	if ((Average + Threshold) >= PassScore)
+	//	{
+	//		printf("합격입니다.\n");
+	//	}
+	//	else
+	//	{
+	//		printf("불합격입니다.\n");
+	//	}
+	//}
+
+
+////1. 세 수 중 최댓값과 최솟값 찾기
+	////	3개의 정수를 입력받아, 그중 가장 큰 수와 가장 작은 수를 출력
+	//int Num1 = 0, Num2 = 0, Num3 = 0;
+	//printf("정수를 3개 입력해 주세요 : ");
+	//cin >> Num1 >> Num2 >> Num3;
+	//int Max = 0;	
+	//if (Num1 >= Num2 && Num1 >= Num3)
+	//{
+	//	Max = Num1;
+	//}
+	//else if ( Num2 >= Num1 && Num2 >= Num3)
+	//{
+	//	Max = Num2;
+	//}
+	//else
+	//{
+	//	Max = Num3;
+	//}
+
+	//int Min = 0;
+	//if (Num1 <= Num2 && Num1 <= Num3)
+	//{
+	//	Min = Num1;
+	//}
+	//else if (Num2 <= Num1 && Num2 <= Num3)
+	//{
+	//	Min = Num2;
+	//}
+	//else
+	//{
+	//	Min = Num3;
+	//}
+	//printf("Max : %d, \tMin : %d", Max, Min);
+
+
+	////2. 세 개의 선분 길이를 입력받아, 이 선분들로 삼각형을 만들 수 있는지 판별하기
+	////	조건: 삼각형이 되려면 '한 변의 길이 < 나머지 두 변의 길이의 합'이어야 함.
+	//int Num1 = 0, Num2 = 0, Num3 = 0;
+	//printf("정수를 3개 입력해 주세요 : ");
+	//cin >> Num1 >> Num2 >> Num3;
+
+	//if (((Num1 + Num2) > Num3) && ((Num1 + Num3) > Num2) && ((Num2 + Num3) > Num1))
+	//{
+	//	printf("삼각형이 성립합니다.\n");
+	//}
+	//else
+	//{
+	//	printf("삼각형이 아닙니다.\n");
+	//}
+
+	//3. 미니 계산기
+	//	두 개의 정수와 하나의 연산자(+, -, *, /)를 입력받아 결과를 출력
+	//	단, 나눗셈에서 0으로 나누려고 하면 "0으로 나눌 수 없습니다"라는 에러 메시지를 출력
+	//int Num1 = 0;
+	//int Num2 = 0;
+	//char Op = 0;
+	//float Result = 0.0f;
+	//cin >> Num1 >> Op >> Num2;
+	//switch (Op)
+	//{
+	//case '+':
+	//	Result = Num1 + Num2;		
+	//	break;
+	//case '-':
+	//	Result = Num1 - Num2;
+	//	break;
+	//case '*':
+	//	Result = Num1 * Num2;
+	//	break;
+	//case '/':
+	//	if (Num2 != 0)
+	//	{
+	//		Result = Num1 / Num2;
+	//	}
+	//	else
+	//	{
+	//		printf("0으로 나눌 수 없습니다.\n");
+	//	}
+	//	break;
+	//default:
+	//	break;
+	//}
+	//if (Op != '/')
+	//{
+	//	printf("%d %c %d = %.0f\n", Num1, Op, Num2, Result);
+	//}
+	//else
+	//{
+	//	if (Num2 != 0)
+	//	{
+	//		printf("%d %c %d = %.2f\n", Num1, Op, Num2, Result);
+	//	}
+	//}
+
+	//4. 윤년 판별기
+	//	연도(예: 2024)를 입력받아 그 해가 윤년인지 평년인지 출력
+	//	윤년의 조건:
+	//	연도가 4로 나누어 떨어지면 윤년이다.
+	//	하지만 100으로 나누어 떨어지면 평년이다.
+	//	그럼에도 400으로 나누어 떨어지면 윤년이다.
+	//int Year = 0;
+	//printf("연도를 입력해 주세요 : ");
+	//cin >> Year;
+	//bool bLeap = (((Year % 4) == 0) && ((Year % 100) != 0)) || (Year % 400 == 0);
+	//if (bLeap)
+	//{
+	//	printf("%d년은 윤년이다.", Year);
+	//}
+	//else
+	//{
+	//	printf("%d년은 평년이다.", Year);
+	//}
