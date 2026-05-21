a419d8e685450bd32172c9afffee54510d6a81ea
go2665 <go2665@outlook.com>
2026-05-21 13:56:17 +0900

    함수 만들기

---
+++ Unreal10th_HealloWorld/Day0521_2.cpp
@@ -0,0 +1,12 @@
+#include "Day0521_2.h"
+
+int Add(int Num1, int Num2)
+{
+	int Result = Num1 + Num2;
+	return Result;
+}
+
+int Sub(int Num1, int Num2)
+{
+	return 0;
+}
---
+++ Unreal10th_HealloWorld/Day0521_2.h
@@ -0,0 +1,10 @@
+#pragma once
+// 해더파일에는 선언만 넣는다.
+
+// Add라는 이름의 함수를 선언 했다.
+// Add는 int Num1과 int Num2 두개의 파라메터를 가지고
+// int 타입의 데이터를 리턴한다.
+int Add(int Num1, int Num2);
+
+
+int Sub(int Num1, int Num2);
