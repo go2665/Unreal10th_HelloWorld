2be0a3db4b1155451759ecf4fba000602bf1d269
go2665 <go2665@outlook.com>
2026-05-28 11:04:17 +0900

    파일입출력

--- Unreal10th_HealloWorld/Day0528.cpp
+++ Unreal10th_HealloWorld/Day0528.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string.h>
 #include "Day0528.h"
 
@@ -76,4 +77,64 @@ void Day0528()
 	// 인덱스로 문자열의 특정 위치 접근하기
 	printf("%s의 첫번째 글자는 %c입니다.\n", Str1.c_str(), Str1[0]);
 	printf("%s의 네번째 글자는 %c입니다.\n", Str1.c_str(), Str1[4]);
+
+	// 간단 실습
+	// std::string과 char를 파라메터로 받아서 string에 char가 어디에 있었는지 출력하는 함수 만들기
+	//	- 여러개 있으면 모두 출력
+	//	- 없으면 없다고 출력
+	FindCharacter("Hello", 'l');
+	FindCharacter(Str1, 'o');
+	FindCharacter(Str1, 'x');
+
+
+	// 파일입력
+	const string FilePath = ".\\Data\\DataFile.txt";	// .은 현재 워킹 폴더(기본적으로 파일이 실행되는 곳)
+	std::ifstream InputFile(FilePath);	// 입력용 파일스트림 만들기
+	if (InputFile.is_open())
+	{
+		std::string FileTexts(
+			(std::istreambuf_iterator<char>(InputFile)),
+			std::istreambuf_iterator<char>()
+		);
+		printf("파일 내용 : \n%s\n", FileTexts.c_str());
+		InputFile.close();
+	}
+	else
+	{
+		// 파일이 없거나 다른 이유로 열리지 않았다.
+		printf("파일을 열 수 없습니다.\n");
+	}
+
+	// 파일출력
+	const string OutputFilePath = ".\\Data\\OutTest.txt";
+	std::ofstream OutFile(OutputFilePath);	// 기존 파일 덮어쓰기
+	//std::ofstream OutFile(OutputFilePath, std::ios::app);	// 덧붙이기
+	if (OutFile.is_open())
+	{
+		OutFile << "Hello World\n";
+		OutFile << "안녕하세요2.\n";
+		OutFile.close();
+	}
+	else
+	{
+		printf("파일을 생성하는데 실패했습니다.");
+	}
+}
+
+void FindCharacter(const std::string& Str, const char Target)
+{
+	bool bFound = false;
+	size_t Pos = Str.find(Target);	// 일단 찾기 시도(Str에서 처음부터 찾아라)
+	while (Pos != string::npos)		// 찾았으면 while안으로 들어가기(npos는 못찾았을 경우)
+	{
+		printf("%s에서 '%c'은 %d 인덱스에 있습니다.\n", Str.c_str(), Target, (int)Pos);
+		Pos = Str.find(Target, Pos + 1);	// Str에서 Target을 찾는데 Pos(이전에 찾은 위치) 다음부터 찾아라. (find의 두번째 파라메터는 시작 위치)
+		bFound = true;	// 찾았다고 표시
+	}
+
+	if (!bFound)
+	{
+		// 찾은것이 없다.
+		printf("%s에서 '%c'는 없습니다.\n", Str.c_str(), Target);
+	}
 }
--- Unreal10th_HealloWorld/Day0528.h
+++ Unreal10th_HealloWorld/Day0528.h
@@ -1,3 +1,6 @@
 #pragma once
 
 void Day0528();
+void FindCharacter(const std::string& Str, const char Target);
+
+void Test(int i = 0);
