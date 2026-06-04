51cd779be48e6049f507507d4ebbce3407c7f05b
go2665 <go2665@outlook.com>
2026-06-04 14:05:31 +0900

    warning 제거

--- Unreal10th_HealloWorld/Orc.cpp
+++ Unreal10th_HealloWorld/Orc.cpp
@@ -3,6 +3,6 @@
 void Orc::Skill(Actor* InTarget) const
 {
 	printf("[%s] : 도끼 던지기 스킬을 사용합니다.\n", Name.c_str());
-	int Damage = CalcDamage() * 3.0f;
+	int Damage = static_cast<int>(CalcDamage() * 3.0f);
 	ApplyDamage(InTarget, Damage);
 }
--- Unreal10th_HealloWorld/Slime.cpp
+++ Unreal10th_HealloWorld/Slime.cpp
@@ -3,6 +3,6 @@
 void Slime::Skill(Actor* InTarget) const
 {
 	printf("[%s] : 슬라임이 산성용액 스킬을 사용합니다.\n", Name.c_str());
-	int Damage = CalcDamage() * 2.5f;
+	int Damage = static_cast<int>(CalcDamage() * 2.5f);
 	ApplyDamage(InTarget, Damage);
 }
